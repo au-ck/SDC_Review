72f408bd1a8218bb7240bacf771ae8a4ceca4acb
MNC <maddinenichaitanya658@gmail.com>
2026-04-21 00:16:47 +0530

    added cors configuring snippet in application.java

--- FARM-SPRINGBOOT/src/main/java/com/klef/FarmSpringbootApplication.java
+++ FARM-SPRINGBOOT/src/main/java/com/klef/FarmSpringbootApplication.java
@@ -2,13 +2,31 @@ package com.klef;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FarmSpringbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FarmSpringbootApplication.class, args);
-		System.out.println("Project Running Sucessfully...");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FarmSpringbootApplication.class, args);
+        System.out.println("Project Running Successfully...");
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "http://54.234.117.205:3000",
+                            "http://localhost:3000"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
